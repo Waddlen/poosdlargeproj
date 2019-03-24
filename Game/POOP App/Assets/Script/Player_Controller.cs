ba0f28f73a50e4c274344cca4ca49f148c3a3cf2
JordyPearl <42579257+JordyPearl@users.noreply.github.com>
2019-03-24 15:17:51 -0400

    Added my scripts and test scene to Unity

---
+++ Game/POOP App/Assets/Script/Player_Controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody2D rb;
+	private Vector2 moveVelocity;
+
+	// Use this for initialization
+	void Start () {
+		
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//Arrow Key Inputs (Ex. x,y = 1,-1)
+		Vector2 moveInput = new Vector2( Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical") );
+		moveVelocity = moveInput.normalized * speed;
+	}
+
+	void FixedUpdate() {
+
+		rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+	}
+}
