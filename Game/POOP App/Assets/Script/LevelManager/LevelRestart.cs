06fefb5224eacf92e5321fe8bc678835430d4e0f
JordyPearl <42579257+JordyPearl@users.noreply.github.com>
2019-03-31 16:50:10 -0400

    Restart button works

---
+++ Game/POOP App/Assets/Script/LevelManager/LevelRestart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelRestart : MonoBehaviour {
+	
+	//public string Level;
+	public void Restart()
+	{
+		//SceneManager.LoadScene(Level);
+		SceneManager.LoadScene(SceneManager.GetActiveScene ().buildIndex);
+	}
+	
+	void Start()
+	{
+		Time.timeScale = 1;
+	}
+}
