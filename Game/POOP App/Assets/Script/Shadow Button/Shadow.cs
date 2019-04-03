eb44026da4ae3f3dc6132c3c30562a017adb30a5
JordyPearl <42579257+JordyPearl@users.noreply.github.com>
2019-04-02 22:20:19 -0400

    WIP adding player shadow

---
+++ Game/POOP App/Assets/Script/Shadow Button/Shadow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shadow : MonoBehaviour {
+
+	private bool Active = false;
+
+	private int TimeLimit = 10;
+
+	public GameObject JS;
+
+	public GameObject Cam;
+
+	public GameObject player;
+
+	public GameObject shadow;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Activate()
+	{
+		Active = true;
+		//Starts where player was
+		shadow.transform.position = player.transform.position;
+		GiveControl(shadow);
+	}
+
+	void GiveControl(Gameobject curPlayer)
+	{
+
+	}
+}
