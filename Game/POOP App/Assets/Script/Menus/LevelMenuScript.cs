374ef1b1fd024d90ca16308bdf3224b63ed98bf7
Christopher S Fleming <30755865+Arlithrien@users.noreply.github.com>
2019-04-03 16:09:40 -0400

    Added assets and meny functionality
    - Added some of the art assets
    - Added main menu
    - Added level select menu

---
+++ Game/POOP App/Assets/Script/Menus/LevelMenuScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	private GUISkin skin;
+
+	void Start()
+	{
+		skin = Resources.Load("GUISkin") as GUISkin;
+	}
+	void OnGUI()
+	{
+		const int buttonWidth = 185;
+		const int buttonHeight = 135;
+		GUI.skin = skin;
+
+		if(
+			GUI.Button(
+
+			new Rect(
+			(27 * Screen.width /100) - (buttonWidth / 2),
+			(33 * Screen.height / 100) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+				),
+				"Level 1"
+				)
+			  )
+
+		{
+			//On click, load level select.
+			SceneManager.LoadScene("Test Scene");
+		}
+		
+	}
+}
---
+++ Game/POOP App/Assets/Script/Menus/MainMenuScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	private GUISkin skin;
+
+	void Start()
+	{
+		skin = Resources.Load("GUISkin") as GUISkin;
+	}
+	void OnGUI()
+	{
+		const int buttonWidth = 250;
+		const int buttonHeight = 100;
+		GUI.skin = skin;
+
+		if(
+			GUI.Button(
+
+			new Rect(
+			Screen.width /2 -(buttonWidth / 2),
+			(53 * Screen.height / 100) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+				),
+				"Level Select"
+				)
+			  )
+
+		{
+			//On click, load level select.
+			SceneManager.LoadScene("LevelSelect");
+		}
+
+		if(
+			GUI.Button(
+
+			new Rect(
+			Screen.width /2 -(buttonWidth / 2),
+			(71 * Screen.height / 100) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+				),
+				"Options"
+				)
+			  )
+		
+				{
+			//On click, load level select.
+			SceneManager.LoadScene("Options");
+		}
+
+
+		if(
+			GUI.Button(
+
+			new Rect(
+			Screen.width /2 -(buttonWidth / 2),
+			(88 * Screen.height / 100) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+				),
+				"Leaderboard"
+				)
+			  )
+		
+				{
+			//On click, load level select.
+			SceneManager.LoadScene("Leaderboard");
+		}
+
+	}
+}
