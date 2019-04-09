15694f03038b1b4afbf56488ee858a24ebf43e67
JordyPearl <42579257+JordyPearl@users.noreply.github.com>
2019-04-09 17:27:13 -0400

    Wall and Pressure Plate

---
+++ Game/POOP App/Assets/Script/DoorController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+
+	public GameObject Door;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OpenDoor(bool active)
+	{
+		if(active)
+		{
+			Door.SetActive(false);
+		}
+		else
+		{
+			Door.SetActive(true);
+		}
+	}
+}
