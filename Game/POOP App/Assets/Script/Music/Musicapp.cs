e7186caf4fef010f841ccc66d82138c741c0a6c1
Maurizio <maurizioaltamura2@knights.ucf.edu>
2019-04-17 13:05:34 -0400

    added music to run continuosly in background throughout app on loop

---
+++ Game/POOP App/Assets/Script/Music/Musicapp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Musicapp : MonoBehaviour
+{
+
+
+    private static Musicapp instance = null;
+
+    public static Musicapp Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        } 
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
+ // any other methods you need
+
+
+
