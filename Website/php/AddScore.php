4fe56ff70e51a545e0b515ff3f0191e7efc78d88
jadebowyer <47097139+jadebowyer@users.noreply.github.com>
2019-04-22 14:48:10 -0400

    Update AddScore.php

--- Website/php/AddScore.php
+++ Website/php/AddScore.php
@@ -12,7 +12,7 @@
 	}
 	else
 	{
-		$sql = "INSERT INTO leaderboard (score_id,device_id,level_id,score,time) VALUES (DEFAULT,'" . $device_id . "','" . $level_id . "','" . $time . "','" . $score . "')";
+		$sql = "INSERT INTO leaderboard (score_id,device_id,level_id,time,score) VALUES (DEFAULT,'" . $device_id . "','" . $level_id . "','" . $time . "','" . $score . "')";
 		if( $result = $conn->query($sql) != TRUE )
 		{
 			returnWithError( $conn->error );
