8ac83c653b5d09218763649b5e88b2a5eb3f12cf
JordyPearl <42579257+JordyPearl@users.noreply.github.com>
2019-04-22 18:31:54 -0400

    Added updateTime and updateLevel to Player.cs

--- Game/POOP App/Assets/Script/DataStorage/Player.cs
+++ Game/POOP App/Assets/Script/DataStorage/Player.cs
@@ -26,4 +26,81 @@ public class Player : MonoBehaviour {
 		L1Time = data.L2Time;
 		L1Time = data.L3Time;
 	}
+
+	public void updateTime(string time, string Lvl)
+	{
+		string myTime = "";
+		switch(Lvl)
+		{
+			case "Level1":
+				myTime = L1Time;
+				break;
+
+			case "Level2":
+				myTime = L1Time;
+				break;
+
+			case "Level3":
+				myTime = L1Time;
+				break;
+		}
+
+		float curTime = 0f;
+		if(!myTime.Equals("0"))
+		{
+			curTime = ToFloat(myTime);
+		}
+
+		float newTime = ToFloat(time);
+
+		if(newTime < curTime || curTime.Equals(0f))
+		{
+			switch(Lvl)
+			{
+				case "Level1":
+					L1Time = time;
+					break;
+
+				case "Level2":
+					L2Time = time;
+					break;
+
+				case "Level3":
+					L3Time = time;
+					break;
+			}
+		}
+		updateLevel(Lvl);
+
+	}
+
+	public void updateLevel(string Lvl)
+	{
+		int myLvl = 1;
+		switch(Lvl)
+		{
+			case "Level1":
+				myLvl = 1;
+				break;
+
+			case "Level2":
+				myLvl = 2;
+				break;
+
+			case "Level3":
+				myLvl = 3;
+				break;
+		}
+		if(myLvl > curLevel)
+		{
+			curLevel = myLvl;
+		}
+	}
+
+	float ToFloat(string str)
+	{
+		string[] token = str.Split(':');
+		float val = (float.Parse(token[0]) * 60) + float.Parse(token[1]);
+		return val;
+	}
 }
