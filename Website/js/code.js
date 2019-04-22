921beaafef533364f5692cf4dcc4990a7dbcc131
jadebowyer <47097139+jadebowyer@users.noreply.github.com>
2019-04-22 16:23:41 -0400

    update to code.js

--- Website/js/code.js
+++ Website/js/code.js
@@ -1,6 +1,5 @@
-var APIRoot = "./PHP/";
-var fileExtension = ".php";
-var menuName = "";
+var menuName = "level_id";
+var boardName = "scoreList";
 
 /*  add to leaderboard.html: 
         - make small changes to text
@@ -11,11 +10,71 @@ var menuName = "";
     add new code.js:
         - include getPosts() to grab drop down menu selection
         - sent back results as {nickname, score}
-        - adjust menuName to reflect name of menu element id
+        - adjust menuName and boardName to reflect name of element ids
 */
+
+function hideOrShow (elementId, showState)
+{
+    var vis = "visible";
+    var dis = "block";
+    if ( !showState)
+    {
+        vis = "hidden";
+        dis = "none";
+    }
+    document.getElementById(elementId).style.visibility=vis;
+    document.getElementById(elementId).style.display=dis;
+}
 
 function getPosts() 
 {
     var search = document.getElementById(menuName).value;
-    
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST","../php/GetScores.php",false);
+    xhr.setRequestHeader("Content-type","application/json; charset=UTF-8");
+    var jsonPayload = '{"Search" : "' + search + '"}';
+
+    try
+    {
+        xhr.onreadystatechange = function()
+        {
+            if (this.readyState == 4 && this.status == 200)
+            {
+                hideOrShow(boardName,true);
+                var str = xhr.responseText;
+                var jsonObject = JSON.parse(str);
+                var table = document.getElementById(boardName);
+                table.deleteTHead();
+                if (str.includes("No Records Found"))
+                {
+                    var newScore = table.createTHead();
+                    var newScoreInfo = newScore.insertRow(0);
+                    newContactInfo.scope = "row";
+                    newContactIndo.insertCell(0).outerHTML = '<th scope="col">No matching contacts found</th>';
+                }
+                var pages = Math.ceil(jsonObject.results.length / 10);
+                for (var i = curPage*10; i < jsonObject.results.length; i++)
+                {
+                    var jsonObjectTwo = jsonObject.results[i];
+                    var error = jsonObjectTwo.error;
+                    if (error == "")
+                    {
+                        var newScore = table.createTHead();
+                        var newScoreInfo = newScore.insertRow(0);
+                        newScoreInfo.scope = "row";
+                        newScoreInfo.value = "1";
+                        newScoreInfo.insertCell(0).outerHTML = '<th scope="col">'+(jsonObject.results.length-i)+"</th>";
+                        newScoreInfo.insertCell(1).outerHTML = '<th scope="col">'+jsonObjectTwo.Nickname+"</th>";
+                        newScoreInfo.insertCell(2).outerHTML = '<th scope="col">'+jsonObjectTwo.Time+"</th>";
+                    }
+                }
+            }
+        };
+        xhr.send(jsonPayload);
+    }
+    catch (err)
+    {
+        document.getElementById("searchResult").innerHTML = err.message;
+        alert("BIG ERROR BRO");
+    }
 }
