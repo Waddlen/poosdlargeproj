1af85e4b29b294f572246f583f322f5d1d985845
JordyPearl <42579257+JordyPearl@users.noreply.github.com>
2019-04-22 17:59:56 -0400

    Saves include best time
    -Will need to have a function for updating best time

--- Game/POOP App/Assets/Script/DataStorage/PlayerData.cs
+++ Game/POOP App/Assets/Script/DataStorage/PlayerData.cs
@@ -10,10 +10,15 @@ public class PlayerData : MonoBehaviour {
 
 	public string email, id;
 
+	public string L1Time, L2Time, L3Time;
+
 	public PlayerData (Player player)
 	{
 		curLevel = player.curLevel;
 		email = player.email;
 		id = player.id;
+		L1Time = player.L1Time;
+		L1Time = player.L2Time;
+		L1Time = player.L3Time;
 	}
 }
