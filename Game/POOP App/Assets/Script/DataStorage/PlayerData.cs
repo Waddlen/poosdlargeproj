2683c265d2f9d45a5f08ef6ea18da4905964a4aa
JordyPearl <42579257+JordyPearl@users.noreply.github.com>
2019-04-22 17:55:17 -0400

    (WIP) Save state on game
    -Added 3 scripts in DataStroage under scripts
    -Should save and load data when called
    -Not sure how to best call the functions

---
+++ Game/POOP App/Assets/Script/DataStorage/PlayerData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData : MonoBehaviour {
+
+	
+	public int curLevel = 1;
+
+	public string email, id;
+
+	public PlayerData (Player player)
+	{
+		curLevel = player.curLevel;
+		email = player.email;
+		id = player.id;
+	}
+}
