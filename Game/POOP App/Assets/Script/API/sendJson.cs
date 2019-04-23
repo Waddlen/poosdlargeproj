342f36856ac17b9dd73a98d94c8c0d9c8872ba58
Lucia Martisovitsova <lucia.martisovitsova@gmail.com>
2019-04-23 13:50:39 -0400

    Created sendUsernameToServer function in sendJson.cs.

--- Game/POOP App/Assets/Script/API/sendJson.cs
+++ Game/POOP App/Assets/Script/API/sendJson.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class sendJson : MonoBehaviour {
 
 	public bool sendRequest = false;
+	public bool changeUsername = false;
 
 	// Use this for initialization
 	void Start () {
@@ -15,18 +16,18 @@ public class sendJson : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-		// level id
-		// device id
-		// time
-		// score
-
 
 		if (sendRequest) {
-			sendInfoToServer("test_deviceId", "test_time", "test_score", "test_levelId" );
+			Debug.Log ("sendInfoToServer gonna be called...");
 			sendRequest = false;
+			sendInfoToServer("test_deviceId", "test_time", "test_score", "test_levelId" );
 		}
 		
+		if (changeUsername) {
+			Debug.Log ("sendUsernameToServer gonna be called...");
+			changeUsername = false;
+			sendUsernameToServer("test_deviceId", "test_nickname");
+		}
 	}
 
 
@@ -69,23 +70,24 @@ public class sendJson : MonoBehaviour {
 		}
 	}
 
-	void sendUsernameToServer(string deviceId, string username) {
+	// Sends a username and deviceId pair to the server
+	void sendUsernameToServer(string deviceId, string nickname) {
 
-		var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://3.89.35.102/php/AddScore.php");
+		var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://3.89.35.102/php/SetNickname.php");
 		httpWebRequest.ContentType = "application/json";
 		httpWebRequest.Method = "POST";
 		httpWebRequest.Credentials = new System.Net.NetworkCredential("poosdadmin", "DontForgetThis321");
 
 		using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
 		{
-			string json = "{\"device_id\":\"test\"," + 	// device_id
-						"\"time\":\"test\"," +			// time
-						"\"score\":\"test\"," +			// score
-						"\"level_id\":\"test\"}"; 		// level_id
+			string json = "{\"device_id\":\"" + deviceId + "\"," + 	// device_id
+						"\"nickname\":\"" + nickname + "\"}";		// username
 
 			streamWriter.Write(json);
 			streamWriter.Flush();
 			streamWriter.Close();
+
+			Debug.Log("json = " + json);
 		}
 
 		var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
